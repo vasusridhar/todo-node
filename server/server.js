cafd54cd4eea2fe6478629c1c87fe4ae36a78044
vasu sridharan <vasusridharan1@gmail.com>
2017-08-31 11:50:44 +1000

    added tests for post and handler for get

--- server/server.js
+++ server/server.js
@@ -1,19 +1,43 @@
 const bodyparser = require('body-parser');
 const express = require('express');
 
-const { moongoose } = require('./db/mongoose')
-const { Todo } = require('./models/Todo');
-const { User } = require('./models/User');
+var {
+    moongoose
+} = require('./db/mongoose')
+var {
+    Todo
+} = require('./models/Todo');
+var {
+    User
+} = require('./models/User');
 
 var app = express();
 app.use(bodyparser.json());
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        return res.send({
+            todos
+        });
+    }, (e) => {
+
+    });
+});
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
-    todo.save().then((doc) => { res.send(doc); }, (error) => { res.status(400).send(error); });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (error) => {
+        res.status(400).send(error);
+    });
 });
 app.listen(3000, () => {
     console.log('started server on port 3000');
 });
+
+module.exports = {
+    app
+};
