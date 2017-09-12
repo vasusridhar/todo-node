7e38076d8899fed19128da4a1fa0c31e2c33a550
vasu sridharan <vasusridharan1@gmail.com>
2017-09-12 13:36:29 +1000

    delte api

--- server/server.js
+++ server/server.js
@@ -59,6 +59,22 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.sendStatus(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.sendStatus(404).send();
+    }
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400);
+  })
+});
+
 app.listen(port, () => {
   console.log(`started server on port ${port}`);
 });
